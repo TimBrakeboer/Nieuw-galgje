c6b9aeb625d940ba54ea5334fde88085545f0f86
Tim Brakeboer <110272@st-maartenscollege.nl>
2021-03-16 14:15:25 +0000

    Intro

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print("Raad de letters en als je het woord weet typ het dan in.")
+print("Welkom bij galgje!")
+print("Raad de letters van het woord en als je het woord denkt te weten,") 
+print("typ het dan in.")
