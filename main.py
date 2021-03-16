be901dcfbf4388901ccdf5a1a674eab7a7db2ae9
Tim Brakeboer <110272@st-maartenscollege.nl>
2021-03-16 14:39:36 +0000

    Streepjes tonen

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import random
 
-print("Welkom bij galgje!")
-print("Raad de letters van het woord en als je het woord denkt te weten,") 
-print("typ het dan in.")
-
-
 
 woordenlijst = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
 
+MogelijkeLetters = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
 
-print( random.choice(woordenlijst))
+GeheimWoord = random.choice(woordenlijst)
+lengtewoord = len(GeheimWoord)
+streepjes = [" - "] *lengtewoord
+
+print("Welkom bij galgje!")
+print("Raad de letters van het woord en als je het woord denkt te weten,") 
+print("typ het dan in.")
+print()
+print("Het woord heeft " + str(lengtewoord) + " letters.")
+print(''.join(streepjes))
