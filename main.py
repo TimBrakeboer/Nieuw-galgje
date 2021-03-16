2989c4b2682b922d2e6918d2b82f983c51821546
Tim Brakeboer <110272@st-maartenscollege.nl>
2021-03-16 14:25:45 +0000

    CPU kiest woord

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import random
+
 print("Welkom bij galgje!")
 print("Raad de letters van het woord en als je het woord denkt te weten,") 
 print("typ het dan in.")
+
+
+
+woordenlijst = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
+
+
+print( random.choice(woordenlijst))
