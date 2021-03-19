cf2dee3d75db877f91f6cfec2c57be05ad375830
Tim Brakeboer <110272@st-maartenscollege.nl>
2021-03-19 11:28:39 +0000

    Pogingen en Hangman toegevoegd

--- main.py
+++ main.py
@@ -1,5 +1,55 @@
 import random
+def spel() :
 
+ Pogingen = 6
+ 
+ def drawgalgjes():
+   if Pogingen == 5:
+        print("Jammer, je hebt nog 4 pogingen over")
+        print("""     ____
+      | \|
+      o  |
+         |
+         |
+         |
+    _____|""")
+   if Pogingen == 4:
+        print("Jammer, je hebt nog 3 pogingen over")
+        print("""     ____
+      | \|
+      o  |
+      |  |
+         |
+         |
+    _____|""")
+   if Pogingen == 3:
+        print("Jammer, je hebt nog 2 pogingen over")
+        print("""     ____
+      | \|
+      o  |
+     /|\ |
+         |
+         |
+    _____|""")
+
+   if Pogingen == 2:
+        print("je hebt nog 1 pogingen over")
+        print("""     ____
+      | \|
+      o  |
+     /|\ |
+     / \ |
+         |
+    _____|""")
+
+   if Pogingen == 1:
+        print("""     ____
+      | \|
+      o  |
+     /|\ |
+     / \ |
+         |
+    _____|""")
 
 woordenlijst = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
 
