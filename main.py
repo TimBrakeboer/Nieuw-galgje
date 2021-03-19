bdd7f59c60e4911b64902c6ae27bc393f683827d
Tim Brakeboer <110272@st-maartenscollege.nl>
2021-03-19 11:23:54 +0000

    Woord goed geraden

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ MogelijkeLetters = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 GeheimWoord = random.choice(woordenlijst)
 lengtewoord = len(GeheimWoord)
 streepjes = [" - "] *lengtewoord
+ja = "ja"
+nee = "nee"
+ 
+game = True
 
 print("Welkom bij galgje!")
 print("Raad de letters van het woord en als je het woord denkt te weten,") 
@@ -15,3 +19,17 @@ print("typ het dan in.")
 print()
 print("Het woord heeft " + str(lengtewoord) + " letters.")
 print(''.join(streepjes))
+print (GeheimWoord)
+while game == True:
+   userGuess = input("raad een letter of het woord: ")
+   if userGuess == GeheimWoord:
+       print ("gefeliciteerd", "je hebt het woord geraden")
+       print()
+       print("Wil je nog een keer spelen? Type dan ja of nee!")
+       print("Goed gedaan!!! je had alleen deze letters nodig om het woord te raden")
+
+
+
+
+
+
